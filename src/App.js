314b1f64700ea05050bc0538d95d8889fa862072
Joe Fairburn <Joe.fairburn1@gmail.com>
2018-10-19 21:27:17 +0100

    Store all history

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React, {
+	Component
+} from 'react';
 import './Styles/App.css';
 import TextList from './Components/TextList.js';
 import Navbar from './Components/Navbar.js';
 import WPM from './Components/WPM.js';
-import Reset from'./Components/Reset.js';
-import EndScreen from'./Components/EndScreen.js';
-import Countdown from'./Components/Countdown.js';
+import Reset from './Components/Reset.js';
+import EndScreen from './Components/EndScreen.js';
+import Countdown from './Components/Countdown.js';
 import axios from 'axios';
 
 class App extends Component {
@@ -14,6 +16,7 @@ class App extends Component {
 	totalLetters = 0;
 	finalWPM = 0;
 	averageCount = 0;
+	historyWPM = [];
 	state = {
 		textBox: '',
 		textToType: this.x.split(' '),
@@ -31,16 +34,18 @@ class App extends Component {
 		averageScore: 0,
 		incorrect: false
 	};
-	
+
 	tickSecond() {
 		let newTime = new Date();
 		newTime = newTime.getTime();
 		const words = this.totalLetters / 5;
-		const seconds = (newTime - this.timer)/1000;
-		const minutes = seconds/60;
-		const WPM = Math.floor(words/minutes); //use either words or pointer
-		this.setState({currentWPM: WPM});
-		
+		const seconds = (newTime - this.timer) / 1000;
+		const minutes = seconds / 60;
+		const WPM = Math.floor(words / minutes); //use either words or pointer
+		this.setState({
+			currentWPM: WPM
+		});
+
 	}
 
 	resetApp = () => {
@@ -71,28 +76,32 @@ class App extends Component {
 		}
 
 	}
-	
+
 
 	getQuote = () => {
 		axios.get('https://talaikis.com/api/quotes/random/')
-		.then(response => {
-			this.x = response.data.quote;
-			if(this.x.length > 140 || this.x.length < 90) {
-			this.getQuote();
-			} else {
-				this.setState({ 
-					textToType: this.x.split(' '),
-					author: '- ' + response.data.author
-				});
-				this.setState({ countdown: 0 });
-			}
-			document.getElementById("inputText").focus();
-		});
+			.then(response => {
+				this.x = response.data.quote;
+				if (this.x.length > 140 || this.x.length < 90) {
+					this.getQuote();
+				} else {
+					this.setState({
+						textToType: this.x.split(' '),
+						author: '- ' + response.data.author
+					});
+					this.setState({
+						countdown: 0
+					});
+				}
+				document.getElementById("inputText").focus();
+			});
 	}
-	
+
 	countdown = () => {
-		if(this.state.countdown !== 0) {
-			this.setState({ countdown: this.state.countdown - 1 });
+		if (this.state.countdown !== 0) {
+			this.setState({
+				countdown: this.state.countdown - 1
+			});
 		} else {
 			clearInterval(this.countdownInterval);
 		}
@@ -101,7 +110,9 @@ class App extends Component {
 	componentDidMount() {
 		this.interval = setInterval(() => this.tickSecond(), 1000);
 		this.getQuote();
-		this.setState({ countdown: 1 });
+		this.setState({
+			countdown: 1
+		});
 
 		localStorage.getItem('topScore') && this.setState({
 			topScore: localStorage.getItem('topScore')
@@ -112,7 +123,9 @@ class App extends Component {
 	}
 
 	mistakeMade = () => {
-		this.setState({incorrect: true});
+		this.setState({
+			incorrect: true
+		});
 		console.log("HE");
 	}
 
@@ -122,21 +135,21 @@ class App extends Component {
 		const word = this.state.textToType[pointer];
 		let textBox = event.target.value;
 		let found = word.indexOf(textBox);
-		if(textBox === ' ') textBox = '';
-		if(this.state.countdown === 0) {
+		if (textBox === ' ') textBox = '';
+		if (this.state.countdown === 0) {
 			this.setState({
 				textBox: textBox, //set textbox to value entered in textbox, syncing textbox w/ state
 				currentlyCorrect: found
 			});
 		}
-		if(textBox.length === 1 && pointer === 0) {
+		if (textBox.length === 1 && pointer === 0) {
 			const currentTime = new Date();
 			this.timer = currentTime.getTime();
 		}
 		//when space + text previously found
 		if (textBox.split('')[word.length] === ' ' && this.state.found === true) {
-			this.totalLetters += word.length + 1;// +1 for space
-			pointer+=1;
+			this.totalLetters += word.length + 1; // +1 for space
+			pointer += 1;
 			this.setState({
 				pointer: pointer, //increment counter
 				textBox: '', //reset text box
@@ -148,59 +161,132 @@ class App extends Component {
 			this.setState({
 				found: true
 			});
-			if(pointer === this.state.textToType.length -1) {
-			this.finalWPM = this.state.currentWPM;
-			let averageScores = this.state.averageScores;	
-			averageScores[this.averageCount] = this.finalWPM;
-			if(this.averageCount !== 4) {
-				this.averageCount += 1;
-			} else {
-				this.averageCount = 0;
-			}
+			if (pointer === this.state.textToType.length - 1) {
+				this.finalWPM = this.state.currentWPM;
+				this.historyWPM.push(this.finalWPM);
+				let averageScoreLength = this.historyWPM;
+				if(this.historyWPM.length > 5) {
+					averageScoreLength = 5;
+				}
+				let averageScores = this.historyWPM.slice(this.historyWPM.length - averageScoreLength, this.historyWPM.length);
+				if (averageScores.length < 5) {
+					averageScoreLength = averageScores.length;
+				} else {
+					averageScoreLength = 5;
+				}
 
-			//new top score
-			if(this.state.topScore < this.finalWPM) {
-				this.setState({topScore: this.finalWPM});
-				localStorage.setItem('topScore', this.finalWPM);
-			}
-
-			let average = (this.state.averageScores.reduce((a, b) => a + b, 0)) / this.state.averageScores.length;//calculate average
-			// DO SOMETHING HERE END 
-			this.setState({ 
-				hideMain: true,
-				hideEndScreen: false,
-				averageScores: averageScores,
-				averageScore: Math.round(average)
-			});
+				//new top score
+				if (this.state.topScore < this.finalWPM) {
+					this.setState({
+						topScore: this.finalWPM
+					});
+					localStorage.setItem('topScore', this.finalWPM);
+				}
+				console.log(averageScores);
+				console.log(averageScoreLength);
+				let average = (averageScores.reduce((a, b) => a + b, 0)) / averageScoreLength; //calculate average
+				// DO SOMETHING HERE END 
+				this.setState({
+					hideMain: true,
+					hideEndScreen: false,
+					averageScores: averageScores,
+					averageScore: Math.round(average)
+				});
 				console.log(this.state.averageScore);
-				
+
 			}
-		
-		}
-		else {
+
+		} else {
 			this.setState({
 				found: false
 			});
 		}
 	}
-	
-  render() {
-    return (
-			<div>
-				<Navbar />
-				<EndScreen hide={this.state.hideEndScreen} wpm={this.finalWPM} startGame = {this.startGame} averageScores = {this.state.averageScores} averageScore = {this.state.averageScore} topScore ={this.state.topScore} />
-				<div className={'text-center content hide-' + this.state.hideMain}>
-					<TextList totalLength = {this.x.length} text = {this.state.textToType} textLength = {this.state.textBox.length} 
-						found = {this.state.found} pointer = {this.state.pointer} 
-						currentlyCorrect = {this.state.currentlyCorrect} author = {this.state.author} />
-					<input id= 'inputText' className = {'textInput mistake-' + this.state.incorrect} value = {this.state.textBox} onChange = {(event) => this.textChangeHandler(event)} autoFocus={true} maxLength = '22' placeholder={this.state.placeholder}/>
-					<Reset reset = {this.resetApp} />
-					<WPM wordsPerMinute = {this.state.currentWPM} textBoxLength = {this.state.textBox.length} />
-				</div>
-				
-			</div>
-    );
-  }
+
+	render() {
+		return ( <div >
+			<
+			Navbar / >
+			<
+			EndScreen hide = {
+				this.state.hideEndScreen
+			}
+			wpm = {
+				this.finalWPM
+			}
+			startGame = {
+				this.startGame
+			}
+			averageScores = {
+				this.state.averageScores
+			}
+			averageScore = {
+				this.state.averageScore
+			}
+			topScore = {
+				this.state.topScore
+			}
+			/> <div className = {
+				'text-center content hide-' + this.state.hideMain
+			} >
+			<
+			TextList totalLength = {
+				this.x.length
+			}
+			text = {
+				this.state.textToType
+			}
+			textLength = {
+				this.state.textBox.length
+			}
+			found = {
+				this.state.found
+			}
+			pointer = {
+				this.state.pointer
+			}
+			currentlyCorrect = {
+				this.state.currentlyCorrect
+			}
+			author = {
+				this.state.author
+			}
+			/> <
+			input id = 'inputText'
+			className = {
+				'textInput mistake-' + this.state.incorrect
+			}
+			value = {
+				this.state.textBox
+			}
+			onChange = {
+				(event) => this.textChangeHandler(event)
+			}
+			autoFocus = {
+				true
+			}
+			maxLength = '22'
+			placeholder = {
+				this.state.placeholder
+			}
+			/> <
+			Reset reset = {
+				this.resetApp
+			}
+			/> <
+			WPM wordsPerMinute = {
+				this.state.currentWPM
+			}
+			textBoxLength = {
+				this.state.textBox.length
+			}
+			/> <
+			/div>
+
+			<
+			/div>
+		);
+	}
 }
 
 export default App;
