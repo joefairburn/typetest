97ec61e1532c981e55dbda89d07ae3c07594bf52
Joe Fairburn <Joe.fairburn1@gmail.com>
2020-02-13 16:34:13 +0000

    remove profile

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,6 @@ const Navbar = (props) => {
 	return (
 		<nav className='navbar'> 
 			<Link className='page-title' to="/">typetest</Link>
-			<Link className='heading' to="/profile">Profile</Link>
 		</nav>
 	);
 }
