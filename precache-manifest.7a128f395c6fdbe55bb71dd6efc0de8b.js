89214a9d00dad0341f99ed0b3d65c983bb70e60f
Joe Fairburn <Joe.fairburn1@gmail.com>
2020-02-15 19:10:46 +0000

    Updates

---
+++ precache-manifest.7a128f395c6fdbe55bb71dd6efc0de8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9b8be4b6df27798ddbc",
+    "url": "/typetest/static/css/main.7b888df0.chunk.css"
+  },
+  {
+    "revision": "e9b8be4b6df27798ddbc",
+    "url": "/typetest/static/js/main.e9b8be4b.chunk.js"
+  },
+  {
+    "revision": "fe18829822c4a5554e55",
+    "url": "/typetest/static/js/runtime~main.fe188298.js"
+  },
+  {
+    "revision": "45adb858d4b0f3d51fbb",
+    "url": "/typetest/static/js/2.45adb858.chunk.js"
+  },
+  {
+    "revision": "99576dbdd09665fe34a92c2994ffee8a",
+    "url": "/typetest/static/media/pencil-alt-solid.99576dbd.svg"
+  },
+  {
+    "revision": "2d28fe22f775a99629fc9e830ff34e7f",
+    "url": "/typetest/index.html"
+  }
+];
